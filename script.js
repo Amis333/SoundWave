904609b3dc94ee62947601c92c9eb50f01e6a1f3
Amoras <vlad172348@gmail.com>
2022-12-12 22:05:29 +0300

    1.3

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ function clickHandler() {
     alert(`Спасибо, ${name}, что выбрали наш сервис, на вашу почту ${email} будет отправленно письмо с подтверждением!` )
     location.href = "endPoint.html"
 }
+
+function subMail() {
+
+    let name = document.getElementById('name').value
+    let text = document.getElementById('message').value
+    console.log(text)
+    location.href = `mailto:vlad172348@gmail.com&body=${text}?subject=${name} спрашивает`
+
+}
